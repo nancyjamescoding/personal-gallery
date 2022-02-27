5c5f300194e10c87ae69441672ba1db2b6ff4354
nancy nyokabi <nancynyokabi1995@gmail.com>
2022-02-27 15:20:30 +0300

    Added multiple images on the page

--- photos/views.py
+++ photos/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from unicodedata import category
+from django.shortcuts import render, redirect
 from django.template import context
 from .models import Category, Photo
 
 # Create your views here.
 def gallery(request):
+    category = request.GET.get('category')
+    if category == None:
+        photos=Photo.objects.all()
+    else:
+        photos=Photo.objects.filter(category__name=category)  
+
     categories = Category.objects.all()
-    photos=Photo.objects.all()
     context = {'categories':categories, 'photos': photos}
     return render(request,'photos/gallery.html', context)
 
